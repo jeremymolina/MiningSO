00ff7de02fc171276a95469a34f382dfe19a6f84
Jeremy Molina <jeremy.mc@cloudera.com>
2019-10-08 18:09:29 -0600

    Initial Commit
    First Commit.

---
+++ cmake-build-debug/CMakeFiles/MiningSO.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MiningSO.dir/main.cpp.o"
+  "MiningSO.pdb"
+  "MiningSO"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MiningSO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <sys/resource.h>
+#include "rss.h"
+#include "maps.h"
+#include <unistd.h>
+#define ERROR -1
+
+static int collectMemData() {
+    return getCurrentRSS()/1024;
+}
+
+
+static int collectCpuData() {
+    return getCurrentCpu();
+}
+
+static int collectNetData() {
+    return 1;
+}
+
+static int collectDiskData() {
+    return 1;
+}
+
+int main()
+{
+
+    printf("> Initializing parent [%d] \n\n", getpid());
+
+    for(int i=0;i<4;i++)
+    {
+        if(fork() == 0)
+        {
+            printf("[son] pid %d from [parent] pid %d\n",getpid(),getppid());
+
+
+            switch (i) {
+                case 0:
+
+                    printf(">> Running Child task [%d] PID=%ld  \n ", i, getpid());
+                    printf("<<< childMem: %u KiB, VS: %u KiB.\n", collectMemData());
+                    break;
+
+                case 1:
+                    printf(">> Running Child task [%d] PID=%ld  \n ", i, getpid());
+                    printf("<<< childCpu: %u.\n", collectCpuData());
+                    break;
+
+                case 2:
+                    printf(">> Running Child task [%d] PID=%ld  \n ", i, getpid());
+                    printf("<<< childNet: %u.\n", collectNetData());
+                    break;
+
+                case 3:
+                    printf(">> Running Child task [%d] PID=%ld  \n ", i, getpid());
+                    printf("<<< childDisk: %u.\n", collectDiskData());
+                    break;
+
+            }
+
+            printf("<< Closing Child task [%d] \n\n", getpid());
+            exit(0);
+
+
+        }
+
+    }
+
+    for(int i=0;i<4;i++)
+        wait(NULL);
+
+    printf("< Finalizing parent [%d] \n\n", getpid());
+
+}
