5a4ccf378e2f19a65bba828a32fca92aad56cc3d
Jeremy Molina <jeremy.mc@cloudera.com>
2019-12-11 16:28:20 -0600

    xcode
    xcode

--- cmake-build-debug/CMakeFiles/MiningSO.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/MiningSO.dir/DependInfo.cmake
@@ -4,9 +4,9 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/jeremymolina/CLion/MiningSO/main.cpp" "/home/jeremymolina/CLion/MiningSO/cmake-build-debug/CMakeFiles/MiningSO.dir/main.cpp.o"
+  "/Users/jeremy.mc/Library/Mobile Documents/com~apple~CloudDocs/CLionProjects/MiningSO/main.cpp" "/Users/jeremy.mc/Library/Mobile Documents/com~apple~CloudDocs/CLionProjects/MiningSO/cmake-build-debug/CMakeFiles/MiningSO.dir/main.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "GNU")
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
